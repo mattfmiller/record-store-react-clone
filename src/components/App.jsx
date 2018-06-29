da7c0086ac2a4a3fca429cd07470b100acad1276
Matt Miller <matt.f.miller@live.com>
2018-06-29 10:19:02 -0700

    Add about component and refact welcome component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Welcome from './Welcome';
+import About from './About';
 import Error404 from './Error404';
 import Footer from './Footer';
 
@@ -11,6 +12,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={Welcome}/>
+        <Route path='/about' component={About}/>
         <Route component={Error404}/>
       </Switch>
       <Footer/>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 function Welcome() {
   return(
     <div>
-      <h1>Welcome to our store!</h1>
+      <h2>Welcome to our store!</h2>
     </div>
   );
 }
