0f5ef12735d8cd9139c2818549029257621cca2e
Matt Miller <matt.f.miller@live.com>
2018-06-29 15:07:03 -0700

    Add styling to welcom component.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Welcome() {
   return(
-    <div>
+    <div className='container'>
+      <style jsx>{`
+          .container {
+            padding: 0 8vw;
+          }
+          .container h2 {
+            font-weight: 400;
+            font-size: 32px;
+            margin-left: auto;
+            margin-right: auto;
+            align-items: center;
+            width: 1110px;
+          }
+        `}
+      </style>
       <h2>Welcome to our store!</h2>
     </div>
   );
