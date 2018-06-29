0f4810b470d75ea4282936c6f8c4362539c7a859
Matt Miller <matt.f.miller@live.com>
2018-06-29 10:08:14 -0700

    Add welcome component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Welcome from './Welcome';
 import Error404 from './Error404';
 import Footer from './Footer';
 
@@ -9,7 +10,8 @@ function App(){
     <div>
       <Header/>
       <Switch>
-        <Route component={Error404} />
+        <Route exact path='/' component={Welcome}/>
+        <Route component={Error404}/>
       </Switch>
       <Footer/>
     </div>
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Welcome() {
+  return(
+    <div>
+      <h1>Welcome to our store!</h1>
+    </div>
+  );
+}
+
+export default Welcome;
