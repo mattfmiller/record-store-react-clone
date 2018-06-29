604b0d15282f684561abfdb4f487a103301a2ad4
Matt Miller <matt.f.miller@live.com>
2018-06-29 10:14:43 -0700

    Add logo to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import epicodusLogo from '../assets/images/epicodus.jpg';
 
 function Header() {
   return(
     <div>
-      <Link to='/'><h3>Epicodus Tunes</h3></Link>
-      <Link to='/about'><h4>About</h4></Link>
-      <Link to='/marketplace'><h4>Marketplace</h4></Link>
+      <div className='nav-bar'>
+        <Link to='/'><h3>Epicodus Tunes</h3></Link>
+        <Link to='/about'><h4>About</h4></Link>
+        <Link to='/marketplace'><h4>Marketplace</h4></Link>
+      </div>
+      <div className='logo'>
+        <img src={epicodusLogo}/>
+        <h1>Epicodus Tunes</h1>
+      </div>
     </div>
   );
 }
